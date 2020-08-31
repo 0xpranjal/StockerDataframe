e8bd7604edf77b1b70923b23bcb304006f643686
Bhard27 <pranjalb2709@gmail.com>
2020-09-01 03:19:33 +0530

    new commit

---
+++ build/lib/StockerDataframe/__init__.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def monthly_gainers():
+
+    dfs = pd.read_html('https://money.rediff.com/gainers/nse/monthly',header=0)
+    for df in dfs[:-1]:
+        print(df)
+
+    df['% Change'] = df['% Change'].str.replace(' ', "")
+    print(df)
+    df.to_csv('monthly_top_gainers.csv', index=False)
+
+
+def daily_gainers():
+    dfs = pd.read_html('https://money.rediff.com/gainers/nse/daily',header=0)
+    for df in dfs[:-1]:
+        print(df)
+
+    df1 = df[['Company', '% Change']]
+    print(df1)
+    df1.to_csv('daily_top_gainers.csv', index=False)
+
+
+    
+def weekly_gainers():    
+    dfs = pd.read_html('https://money.rediff.com/gainers/nse/weekly',header=0)
+    for df in dfs[:1]:
+        print(df)
+
+
+    df['% Change'] = df['% Change'].str.replace(' ', "")
+    print(df)
+    df.to_csv('weekly_top_gainers.csv',index=False)
---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+setup(name="StockerDataframe", 
+    version="0.3", 
+    description="A tool which can help you with Stock analysis in an efficient way.",
+    long_description="Long description about the package",author="Pranjal", packages=['StockerDataframe'],install_requires=[])
