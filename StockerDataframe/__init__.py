e961ae11704c2394f0eeac97992fc0856e907f2d
Bhard27 <pranjalb2709@gmail.com>
2020-09-01 07:14:05 +0530

    new commit

--- StockerDataframe/__init__.py
+++ StockerDataframe/__init__.py
@@ -53,3 +53,12 @@ def weekly_losers():
     df1 = df[['Company', '% Change']]
     print(df1)  
     df1.to_csv('weekly_top_losers.csv', index=False)
+
+df monthly_losers():
+    dfs = pd.read_html('https://money.rediff.com/losers/nse/monthly',header=0)
+    for df in dfs[:-1]:
+        print(df)
+
+    df1 = df[['Company', '% Change']]
+    print(df1)  
+    df.to_csv('monthly_top_losers.csv', index=False)
