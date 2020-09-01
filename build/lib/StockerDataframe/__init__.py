b28992fb820b3a4e5909d7c549ae34e24ca4c1d8
Bhard27 <pranjalb2709@gmail.com>
2020-09-01 23:49:36 +0530

    new commit

--- build/lib/StockerDataframe/__init__.py
+++ build/lib/StockerDataframe/__init__.py
@@ -33,3 +33,32 @@ def weekly_gainers():
     df['% Change'] = df['% Change'].str.replace(' ', "")
     print(df)
     df.to_csv('weekly_top_gainers.csv',index=False)
+
+
+def daily_losers():
+    dfs = pd.read_html('https://money.rediff.com/losers/nse/daily',header=0)
+    for df in dfs[:-1]:
+        print(df)
+
+    df1 = df[['Company', '% Change']]
+    print(df1)  
+    df1.to_csv('daily_top_losers.csv', index=False)
+
+
+def weekly_losers():
+    dfs = pd.read_html('https://money.rediff.com/losers/nse/weekly',header=0)
+    for df in dfs[:-1]:
+        print(df)
+
+    df1 = df[['Company', '% Change']]
+    print(df1)  
+    df1.to_csv('weekly_top_losers.csv', index=False)
+
+def monthly_losers():
+    dfs = pd.read_html('https://money.rediff.com/losers/nse/monthly',header=0)
+    for df in dfs[:-1]:
+        print(df)
+
+    df1 = df[['Company', '% Change']]
+    print(df1)  
+    df.to_csv('monthly_top_losers.csv', index=False)
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 setup(name="StockerDataframe", 
-    version="0.3", 
-    description="A tool which can help you with Stock analysis in an efficient way.",
-    long_description="Long description about the package",author="Pranjal", packages=['StockerDataframe'],install_requires=[])
+    version="0.7", 
+    description="A unique tool for better analysis of Stock Market", license='GPL-3.0 License', 
+    long_description="One of the key factors for traders is tracking the best and worst performing stocks over a period of time. With StockerDataframe, you can easily scrape the web for a lot of stock related data which can help you with detailed analysis of the market.",author="Pranjal", packages=['StockerDataframe'], url = 'https://github.com/Bhard27/StockerDataframe', author_email='pranjal27bhardwaj@gmail.com')
