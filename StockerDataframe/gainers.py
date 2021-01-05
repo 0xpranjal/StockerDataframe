e1cacb338a233e1cebd98db63475c53c26f49882
Bhard27 <pranjalb2709@gmail.com>
2021-01-05 21:36:29 +0530

    code enhanced

--- StockerDataframe/gainers.py
+++ StockerDataframe/gainers.py
@@ -7,34 +7,33 @@ def monthly_gainers():
 
     dfs = pd.read_html('https://money.rediff.com/gainers/bse/monthly',header=0)
     for df in dfs[:-1]:
-        print(df)
- 
+        return df
+
     df['% Change'] = df['% Change'].str.replace(' ', "")
     df1 = df[['Company', '% Change']]
-    print(df1)
+    return df1
     df1.to_csv('monthly_top_gainers.csv', index=False)
 
 
 def daily_gainers():
     dfs = pd.read_html('https://money.rediff.com/gainers/bse/daily',header=0)
     for df in dfs[:-1]:
-        print(df)
+        return df
 
     df['% Change'] = df['% Change'].str.replace(' ', "")
     df1 = df[['Company', '% Change']]
-    print(df1)
+    return df1
     df1.to_csv('daily_top_gainers.csv',index=False)
 
 
 def weekly_gainers():
     dfs = pd.read_html('https://money.rediff.com/gainers/bse/weekly',header=0)
     for df in dfs[:1]:
-        print(df)
-
+        return df
 
     df['% Change'] = df['% Change'].str.replace(' ', "")
     df1 = df[['Company', '% Change']]
-    print(df1)
+    return df1
     df1.to_csv('weekly_top_gainers.csv',index=False)
 
 def plot_daily_gainers():
@@ -49,7 +48,7 @@ def plot_daily_gainers():
 
 
 def plot_weekly_gainers():
-    
+
     weekly_gainers()
     plt.style.use('fivethirtyeight')
     data_weekly_gainers = pd.read_csv('weekly_top_gainers.csv')
@@ -59,7 +58,7 @@ def plot_weekly_gainers():
     plt.show(x2)
 
 def plot_monthly_gainers():
-    
+
     monthly_gainers()
     plt.style.use('fivethirtyeight')
     data_monthly_gainers = pd.read_csv('monthly_top_gainers.csv')
